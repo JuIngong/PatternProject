5c75d4e59b60daf0b712468d22c26ddf0f1c100f
Florian Muller <flolaris@gmail.com>
2017-01-10 13:59:18 +0100

    add file

--- src/main/java/Visiteur/AFormeGéométrique.java
+++ src/main/java/Visiteur/AFormeGéométrique.java
@@ -1,9 +1,22 @@
 package Visiteur;
 
+import java.awt.*;
+
 /**
  * Class created on 10/01/2017
  *
  * @author JuIngong
  */
 public  abstract class AFormeGéométrique {
+
+    private Point point;
+    private Rectangle boundingBox;
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
 }
