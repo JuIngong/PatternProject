8fce33903492ffbb0767ba2bd000e2d4759961f8
Frederic Testaniere <frederic.testaniere@etu.unice.fr>
2017-01-10 14:25:14 +0100

    Geoforme Rectangle, Cercle, Carre

---
+++ src/main/java/Visiteur/MyRectangle.java
@@ -0,0 +1,18 @@
+package Visiteur;
+
+import java.awt.*;
+
+/**
+ * Created by frederic on 10/01/2017.
+ */
+public class MyRectangle extends AFormeGéométrique {
+
+    private int longeur;
+    private int largeur;
+
+    public MyRectangle(Point point, int longeur, int largeur){
+        super(point, new Rectangle(longeur, largeur));
+        this.longeur = longeur;
+        this.largeur = largeur;
+    }
+}
