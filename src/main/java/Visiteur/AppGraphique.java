938a6fe395940c32d037c08a3bcfd70df25e0c46
Frédéric Testaniere <frederic.testaniere@etu.unice.fr>
2017-01-15 16:55:41 +0100

    commit

--- src/main/java/Visiteur/AppGraphique.java
+++ src/main/java/Visiteur/AppGraphique.java
@@ -13,13 +13,16 @@ public class AppGraphique extends Applet {
     public void paint(Graphics g){
         FigureDrawer drawer = new FigureDrawer(g);
         List<AFormeGéométrique> formes = new ArrayList();
+
         Cercle c = new Cercle(new Point(50,30), 20);
         MyRectangle r = new MyRectangle(new Point(50,50), 20,20);
         Ligne l = new Ligne(new Point(70,70), new Point(90,30));
+        Texte t = new Texte(new Point(100,100), "TP Visiteur Patron");
 
         formes.add(c);
         formes.add(r);
         formes.add(l);
+        formes.add(t);
         
         for(AFormeGéométrique f : formes){
             f.accept(drawer);
--- src/main/java/Visiteur/FigureDrawer.java
+++ src/main/java/Visiteur/FigureDrawer.java
@@ -1,6 +1,7 @@
 package Visiteur;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 /**
  * Created by frederic on 10/01/2017.
@@ -16,18 +17,22 @@ public class FigureDrawer implements IFigureVisitor {
     }
 
     public void visit(Cercle c) {
+        c.generateBoundingBoc(new Rectangle(c.getPoint().x, c.getPoint().y, c.getRayon(), c.getRayon()));
         g.drawOval(c.getPoint().x, c.getPoint().y, c.getRayon(), c.getRayon());
     }
 
     public void visit(MyRectangle r) {
+        r.generateBoundingBoc(new Rectangle(r.getPoint().x, r.getPoint().y, r.getLongeur(), r.getLargeur()));
         g.drawRect(r.getPoint().x, r.getPoint().y, r.getLongeur(), r.getLargeur());
     }
 
     public void visit(Carre s) {
+        s.generateBoundingBoc(new Rectangle(s.getPoint().x, s.getPoint().y, s.getCote(), s.getCote()));
         g.drawRect(s.getPoint().x, s.getPoint().y, s.getCote(), s.getCote());
     }
 
     public void visit(Ellipse e) {
+        e.generateBoundingBoc(new Rectangle(e.getPoint().x, e.getPoint().y, e.getHauteur(), e.getLargeur()));
         g.drawOval(e.getPoint().x, e.getPoint().y, e.getHauteur(), e.getLargeur());
     }
 
@@ -36,6 +41,8 @@ public class FigureDrawer implements IFigureVisitor {
     }
 
     public void visit(Texte t) {
+        Rectangle2D rec = g.getFontMetrics().getStringBounds(t.getTexte(), g);
+        t.generateBoundingBoc(new Rectangle(t.getPoint().x, t.getPoint().y, (int)rec.getWidth(), (int)rec.getHeight()));
         g.drawString(t.getTexte(), t.getPoint().x, t.getPoint().y);
     }
 }
